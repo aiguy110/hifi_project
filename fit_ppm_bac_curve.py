2b1aa64902d1242d9032595f382d05ee6754b298
Josiah Hunsinger <josiah.hunsinger@gmail.com>
2017-12-15 06:15:42 -0500

    reading bac

--- fit_ppm_bac_curve.py
+++ fit_ppm_bac_curve.py
@@ -5,37 +5,40 @@ from scipy.optimize import minimize
 
 in_file = 'ethanol_sensor.csv'
 bacs = []
-voltages = []
-highest_voltage = -1
+ppms = []
+highest_ppm = -1
 with open(in_file, 'r') as f:
     for i, line in enumerate(f):
         if i == 0: # Skip the the first row. It holds the labels
             continue
         bac, raw_level = list(map(float, line.split(',')))
-        voltage = 5*raw_level/1024
+        voltage = raw_level / 1024 * 5
+        ppm = np.exp(-14.7*((5/3.3 - 1)/(5/voltage - 1) - 0.45))
         bacs.append(bac)
-        voltages.append(voltage)
-        if voltage > highest_voltage:
-            highest_voltage = voltage
+        ppms.append(ppm)
+        if ppm > highest_ppm:
+            highest_ppm = ppm
 
 
 def line_error(params):
     m, b = params
     total_error = 0
     for i in range(len(bacs)):
-        total_error += abs(m*raw_levels[i]+b - bacs[i])
+        total_error += abs(m*ppms[i]+b - bacs[i])
     return total_error
 
 best_m, best_b = minimize(line_error, [0.0, 0.0]).x
 
 # Plot points
-plt.plot(voltages, bacs, 'o')
+plt.plot(ppms, bacs, 'o')
 
 # Plot line
-x = np.array([0, highest_level*1.2])
-print( x,best_m, best_b)
-print(x*best_m)
+x = np.array([0, highest_ppm*1.2])
+print( x,best_m, best_b )
+print( x*best_m )
 y = best_m*x + best_b
 plt.plot(x,y, '-')
-
+plt.ylabel('BAC (%)')
+plt.xlabel('ethanol (ppm)')
+plt.title(f'm={best_m:.{4}}, b={best_b:.{4}}')
 plt.show()
--- live_read.py
+++ live_read.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 import serial
+import time
 
 port = input('Which serial port would you like to connect to? (/dev/ttyACM0)\n>')
 port = '/dev/ttyACM0'
@@ -10,18 +11,49 @@ ser = serial.Serial(port)
 gas_vals = []
 temp_vals = []
 
+bac_per_ppm = 0.0003404
+
+V_source = 5
+V_o = None
+
 #fig = plt.figure()
 #plt.ion()
 
 #def animate(i):
 
+bac_log = []
+log_length = 100
+
+timer = 5
+tic = time.time()
+print(f'Grabbing reference reading in \n{timer}')
 while True:
+    # Tic toc
+    if time.time() - tic > 1:
+        tic = time.time()
+        timer -= 1
+        if timer > 0:
+            print(timer)
+
     try:
         raw = ser.readline()
         name, val = raw.decode('ascii').split('=')
         if name == 'gasValueRaw':
-            gas_vals.append(int(val))
-            print(int(val))
+            V_sensor = float(val) / 1024 * V_source
+
+            if timer == 0 and V_o is None:
+                V_o = V_sensor
+                print('Reference set!')
+                print(f'V_o={V_o}')
+
+            if V_o is not None:
+                ppm = np.exp(-14.7*((V_source/V_o - 1)/(V_source/V_sensor - 1) - 0.45))
+                bac = bac_per_ppm * ppm
+                bac_log.append(bac)
+                if bac_log > log_length:
+                    bac_log.pop(0)
+                print(f'bac={max(bac_log)}       ', end='\r')
+
         elif name == 'tempValueRaw':
             temp_vals.append(int(val))
 
